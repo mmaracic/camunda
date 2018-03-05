d32166c4d6a86b2231f04af6a319d5fbc8673214
CROZ_mmaracic <mmaracic@croz.net>
2018-03-05 03:45:28 +0100

    Oracle configuration

--- jmeter/sql.sql
+++ jmeter/sql.sql
@@ -2,3 +2,7 @@ truncate table dta_token_statistic cascade;
 truncate table dta_token_texts cascade;
 truncate table dta_token cascade;
 truncate table dta_text cascade;
+
+--Oracle
+CREATE USER camunda IDENTIFIED BY camunda;
+GRANT CONNECT, RESOURCE, DBA TO camunda;
